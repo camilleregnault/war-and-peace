23df6d4106561e46644e6d90f1325f8cc321566f
Camille Regnault <c.regnault.pro@gmail.com>
2019-09-18 18:55:50 +0200

    create candidates controller and CRUD

---
+++ app/models/candidate.rb
@@ -0,0 +1,5 @@
+class Candidate < ApplicationRecord
+  validates :name, :popularity_points, :attack_strength, :photo, presence: true
+  validates :popularity_points, numericality: { greater_than: 10, less_than: 100 }
+  validates :attack_strength, numericality: { greater_than: 0, less_than: 20 }
+end
