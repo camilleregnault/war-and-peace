c56913eb88fc5ce5a460415df74db440c963965f
Camille Regnault <c.regnault.pro@gmail.com>
2019-09-20 21:01:57 +0200

    create fight form

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -1,7 +1,12 @@
 class FightsController < ApplicationController
   def index
+    @candidates = Candidate.all
   end
 
   def show
+  end
+
+  def create 
+    redirect_to fights_show_path
   end
 end
