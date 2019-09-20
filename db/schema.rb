f53d4ecc819d1e23cf504eba9e492fd7c81800bc
Camille Regnault <c.regnault.pro@gmail.com>
2019-09-20 14:14:30 +0200

    jquery fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_154610) do
+ActiveRecord::Schema.define(version: 2019_09_20_115356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
