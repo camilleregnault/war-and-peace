dc585fbc6d0a57a8dd69907a7938cd31cefc50f4
Camille Regnault <c.regnault.pro@gmail.com>
2019-09-22 17:26:43 +0200

    add weapons

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -19,26 +19,35 @@ class FightsController < ApplicationController
   end
 
   def history
-    @fights = Fight.last(10)
+    fights = Fight.last(10)
+    @fight_summaries = []
+    fights.each do |fight|
+      summary = {}
+      summary['winner'] = Candidate.find(fight['winner_id'])
+      summary['winner_nb_attack'] = fight['winner_nb_attack']
+      summary['winner_weapon'] = Weapon.find(fight['winner_weapon_id'])
+      summary['looser'] = Candidate.find(fight['looser_id'])
+      summary['looser_nb_attack'] = fight['looser_nb_attack']
+      summary['looser_weapon'] = Weapon.find(fight['looser_weapon_id'])
+      summary['detail'] = fight['detail']
+      @fight_summaries.push(summary)
+    end
   end
 
   private
-
-  def add_weapon(a, b)
-    weapon_a = Weapon.find(params['fight']['weapon_a'])
-    weapon_b = Weapon.find(params['fight']['weapon_b'])
-    a.attack_strength += weapon_a.attack
-    a.popularity_points += weapon_a.defence
-    b.attack_strength += weapon_b.attack
-    b.popularity_points += weapon_b.defence
-  end
 
   def fight_result(fight)
     a = {}
     b = {}
     a['fighter'] = Candidate.find(fight['candidate_a'])
+    a['weapon'] = Weapon.find(fight['weapon_a'])
+    a['fighter'].attack_strength += a['weapon'].attack
+    a['fighter'].popularity_points += a['weapon'].defence
     a['nb_attack'] = 0
     b['fighter'] = Candidate.find(fight['candidate_b'])
+    b['weapon'] = Weapon.find(fight['weapon_b'])
+    b['fighter'].attack_strength += b['weapon'].attack
+    b['fighter'].popularity_points += b['weapon'].defence
     b['nb_attack'] = 0
 
     loop do
@@ -55,10 +64,12 @@ class FightsController < ApplicationController
   def set_result_fight(winner, looser)
     fight = {}
     fight['winner_id'] = winner['fighter'].id
+    fight['winner_weapon_id'] = winner['weapon'].id
     fight['winner_nb_attack'] = winner['nb_attack']
     fight['looser_id'] = looser['fighter'].id
+    fight['looser_weapon_id'] = looser['weapon'].id
     fight['looser_nb_attack'] = looser['nb_attack']
-    fight['detail'] = fight_detail(winner['fighter'], looser['fighter'], fight)
+    fight['detail'] = fight_detail(winner, looser, fight)
     fight
   end
 
@@ -70,17 +81,21 @@ class FightsController < ApplicationController
       break unless winner_attack.positive?
 
       if winner_attack.positive?
-        detail.push("#{winner.name} attaque #{looser.name}.
-          #{looser.name} perd #{winner.attack_strength} points de vie.")
+        detail.push("#{winner['fighter'].name} attaque #{looser['fighter'].name}
+          avec #{winner['weapon'].name}.
+          #{looser['fighter'].name} perd #{winner['fighter'].attack_strength}
+          points de vie.")
         winner_attack -= 1
       end
 
       if looser_attack.positive?
-        detail.push("#{looser.name} attaque #{winner.name}.
-          #{winner.name} perd #{looser.attack_strength} point de vie.")
+        detail.push("#{looser['fighter'].name} attaque #{winner['fighter'].name}
+          avec #{looser['weapon'].name}.
+          #{winner['fighter'].name} perd #{looser['fighter'].attack_strength}
+          point de vie.")
         looser_attack -= 1
       else
-        detail.push("#{looser.name} est mort.")
+        detail.push("#{looser['fighter'].name} est mort.")
       end
     end
     detail
